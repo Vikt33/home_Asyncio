c6aaeeec283f2bf9d05d57af681f6ffa000c2985
Viktor <fomvaf@mail.ru>
2022-04-24 12:48:21 +0300

    New

---
+++ config.py
@@ -0,0 +1,8 @@
+DB_FILE = './contacts.db'
+
+HOST = "smtp.gmail.com"
+PORT = 1234
+USERNAME = "test"
+PASSWORD = "12345"
+
+
---
+++ sender.py
@@ -0,0 +1,11 @@
+from email.message import EmailMessage
+import aiosmtplib
+from config import HOST, PORT, USERNAME, PASSWORD
+
+async def send_email( email, subject, text):
+    message = EmailMessage()
+    message["From"] = "Python developer"
+    message["To"] = email
+    message["Subject"] = subject
+    message.set_content(text)
+    await aiosmtplib.send(message, hostname=HOST, port=PORT, username=USERNAME, password=PASSWORD)
